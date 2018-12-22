599e11606553fc3863319207b3605b041b232dce
Ken Weed <kenneth.weed@instacart.com>
2018-12-22 10:29:06 -0800

    messaging app

---
+++ app/controllers/chatroom_controller.rb
@@ -0,0 +1,25 @@
+class ChatroomController < ApplicationController
+
+    def index
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def create
+    end
+    
+    def update
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+    
+
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+    def index
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def create
+    end
+    
+    def update
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+end
